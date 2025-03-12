2a5a7cfb0ba7dec9b37906d23d98837a25d109c4
Dave <16257495+DaveKT@users.noreply.github.com>
2025-03-12 17:03:20 -0400

    Create pratchett.js
    Used to add the GNU Terry Pratchett to your write.as blog.

---
+++ pratchett.js
@@ -0,0 +1,6 @@
+(function() {
+    var meta = document.createElement('meta');
+    meta.httpEquiv = "X-Clacks-Overhead";
+    meta.content = "GNU Terry Pratchett";
+    document.getElementsByTagName('head')[0].appendChild(meta);
+})();
